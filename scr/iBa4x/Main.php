9c241c91722fca9043572bab7a0f1143038f37d6
iBa4x <iiBa4x@gmail.com>
2016-08-03 12:48:46 +0300

    Update Main.php

--- scr/iBa4x/Main.php
+++ scr/iBa4x/Main.php
@@ -9,67 +9,66 @@ use pocketmine\event\Listender;
 use pocketmine\event\PlayerMoveEvent;
 use pocketmine\event\PlayerJoinEvent;
 use pocketmine\event\PlayerDeathEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\utlis\Config as C;
 use pocketmine\utlis\TextFormat as T;
 
 Class Main extend PluginBasc implements Listender{
+  
   public function onEnable(){
    $this->getServer()->getPluginManager()->registerEvents($this, $this);
-   $this->getLogger()->info(T::GREEN . "Load HUD1b by iBa4x .");
+   $this->getLogger()->info(T::GREEN . "Load HUD1b by iBa4x.");
    @mkdir($this->getDetaFolder());
   }
-  public function onJoin(PlayerJoinEvent $e){
-   $p = $e->getPlayer();
-   $n = $p->getName();
-   $d = new Config($this->getDetaFolder() . $n . ".yml", C::YAML);
-    if($d->get("K") == null){
-     $d->set("K",0);
-     $d->save();
+  
+  public function onJoin(PlayerJoinEvent $event){
+   $player = $event->getPlayer();
+   $name = $player->getName();
+   $deta = new Config($this->getDetaFolder() . $name . ".yml", C::YAML);
+    if($deta->get("K") == null){
+     $deta->set("K",0);
+     $deta->save();
     }
-    if($d->get("D") == null){
-     $d->set("D",0);
-     $d->save();
+    if($deta->get("D") == null){
+     $deta->set("D",0);
+     $deta->save();
     }
-    if($d->get("S") == null){
-     $d->set("S",0);
-     $d->save();
-    }
-  }
-  public function onDeath(PlayerDeathEvent $e){
-   $en = $e->getEntity();
-   $c = $en->getLastDamageCause();
-    if($en instanceof Player){
-     $n = $e->getEntity()->getName();
-     $d = new Config($this->getDetaFolder() . $n . ".yml", C::YAML);
-      $d->set("D",$d->get("D") + 1);
-      $d->save();
-    }
-    if($c instanceof Player){
-     $n = $killer()->getName();
-     $d = new Config($this->getDetaFolder() . $n . ".yml", C::YAML);
-      $d->set("K",$d->get("K") + 1);
-      $d->save();
-    }
-    if($en instanceof Player){
-     $n = $e->getEntity()->getName();
-     $d = new Config($this->getDetaFolder() . $n . ".yml", C::YAML);
-      $d->set("S",$d->get("S") - 5);
-      $d->save();
-    }
-    if($c instanceof Player){
-     $n = $killer->getName();
-     $d = new Config($this->getDetaFolder() . $n . ".yml", C::YAML);
-      $d->set("S",$d->get("S") + 5);
-      $d->save();
+    if($deta->get("S") == null){
+     $deta->set("S",0);
+     $deta->save();
     }
   }
-  public function onMove(PlayerMoveEvent $e){
-   $p = $e->getPlayer();
-   $n = $p->getName();
-   $cfg = new Config($this->getDetaFolder() . $n . ".yml", C::YAML);
+  
+  public function onDeath(PlayerDeathEvent $event){
+   $entity = $event->getEntity();
+   $cause = $entity->getLastDamageCause();
+    if($entity instanceof Player){
+     $name = $event->getEntity()->getName();
+     $deta = new Config($this->getDetaFolder() . $name . ".yml", C::YAML);
+      $deta->set("D",$deta->get("D") + 1);
+      $deta->set("S",$deta->get("S") - 5);
+      $deta->save();
+    }
+    if($cause instanceof EntityDamageByEntityEvent){
+      $killer = $event->getEntity()->getLastDamageCause()->getDamage();
+      
+      if($killer instanceof Player){
+        $name = $killer()->getName();
+        $deta = new Config($this->getDetaFolder() . $name . ".yml", C::YAML);
+        $deta->set("K",$deta->get("K") + 1);
+        $deta->set("S",$deta->get("S") + 5);
+        $deta->save();
+      }
+    }
+  }
+  
+  public function onMove(PlayerMoveEvent $event){
+   $player = $event->getPlayer();
+   $name = $player->getName();
+   $cfg = new Config($this->getDetaFolder() . $name . ".yml", C::YAML);
    $k = $cfg->get("K");
    $d = $cfg->get("D");
    $s = $cfg->get("S");
-    $p->sendTip(T::GREEN . "                                                                                           -+=+-" . "\n                                                                                          Kills: " . $k . "\n                                                                                          Death" . $d . "\n                                                                                          Score" . $s . "\n                                                                                           -+=+-");
+   $player->sendTip(T::GREEN . "                                                             -+=+-" . "\n                                                            Kills: " . $k . "\n                                                            Death" . $d . "\n                                                            Score" . $s . "\n                                                             -+=+-");
   }
- }
+}
